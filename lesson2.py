161a946183897586a3c3dfcad84934632fe11bb7
dashixss <dasaradomska@gmail.com>
2023-11-30 17:30:53 +0200

    заповнили

--- lesson2.py
+++ lesson2.py
@@ -1,7 +1,12 @@
 class Group_1012:
-    pass
-student1 = Group_1012()
-student2 = Group_1012()
-student3 = Group_1012()
-student4 = Group_1012()
-student5 = Group_1012()
+    def __init__(self, name, gen, avarage_score, age):
+        self.name = name
+        self.gen = gen
+        self.avarage_score = avarage_score
+        self.age = age
+
+student1 = Group_1012("Гончарук Олександр", "чол.", 12, 12)
+student2 = Group_1012("Трофимишин Владислав", "чол", 12,12)
+student3 = Group_1012("Радомська Дарина",  "жін.",  12,  13)
+student4 = Group_1012("Юраш Максим","чол.", 14, 10)
+student5 = Group_1012("Коваленко Макар", "чол", 13, 11.1)
