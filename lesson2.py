40b7b2300a19995578a01f567af14293a3bf98c1
dashixss <dasaradomska@gmail.com>
2023-11-30 17:50:00 +0200

    the end

--- lesson2.py
+++ lesson2.py
@@ -7,7 +7,10 @@ class Group_1012:
         self.age +=1
     def study(self):
         print("Learn good")
-        self.avarage_score += 0.1
+        if self.avarage_score < 12:
+            self.avarage_score += 0.1
+        else:
+            print("U are the best already, slow down")
 
 student1 = Group_1012("Гончарук Олександр", "чол.", 12, 12)
 student2 = Group_1012("Трофимишин Владислав", "чол", 12,12)
@@ -16,7 +19,5 @@ student4 = Group_1012("Юраш Максим","чол.", 14, 10)
 student5 = Group_1012("Коваленко Макар", "чол", 13, 11.1)
 print("Студента #2 в списку звати ", student2.name)
 print("Середня оцінка студента №3 -", student3.avarage_score)
-student5.study()
-student5.study()
-student5.study()
-print(student5.avarage_score)
+student2.study()
+print(student2.avarage_score)
