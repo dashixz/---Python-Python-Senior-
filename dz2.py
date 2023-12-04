3731ce5b9c4a153661355d28ecf84894ab895743
dashixss <dasaradomska@gmail.com>
2023-12-04 17:04:05 +0200

    finished with cats trying smth interesting

--- dz2.py
+++ dz2.py
@@ -7,6 +7,6 @@ class cats_life:
         self.age +=1
         self.breed = breed
 cat1 = cats_life("Fluffy", "girl", 1, "A rugged-looking coat designed for cold Scandinavian winters, almond-shaped eyes, calm demeanor", "Norwegian Forest Cat")
-cat1 = cats_life("", "boy", 2, "Round head, small ears, big eyes and a long, luxurious coat; they are quiet and easy-going", "Persian")
-cat1 = cats_life("", "", 1, "", "")
-cat1 = cats_life("", "", 1, "", "")
+cat2 = cats_life("Snowflake", "girl", 2, "Round head, small ears, big eyes and a long, luxurious coat; they are quiet and easy-going", "Persian")
+cat3 = cats_life("Moonlight", "boy", 0, "Blue eyes with long, light-colored coats. Easy disposition, gentle and friendly with animals and humans. ", "Ragdoll")
+cat4 = cats_life("Shadow", "boy", 6, "Intelligent, kind, and companionable, with about 75 different color combinations ", "Maine Coon Cat")
