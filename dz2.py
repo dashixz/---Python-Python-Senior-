b8e61fdbecff6f543cde9c9a96ab9d2242ca8332
dashixss <dasaradomska@gmail.com>
2023-12-04 17:08:10 +0200

    finished with cats trying smth interesting finished work

--- dz2.py
+++ dz2.py
@@ -10,3 +10,6 @@ cat1 = cats_life("Fluffy", "girl", 1, "A rugged-looking coat designed for cold S
 cat2 = cats_life("Snowflake", "girl", 2, "Round head, small ears, big eyes and a long, luxurious coat; they are quiet and easy-going", "Persian")
 cat3 = cats_life("Moonlight", "boy", 0, "Blue eyes with long, light-colored coats. Easy disposition, gentle and friendly with animals and humans. ", "Ragdoll")
 cat4 = cats_life("Shadow", "boy", 6, "Intelligent, kind, and companionable, with about 75 different color combinations ", "Maine Coon Cat")
+print("Our first cat breed is -", cat1.name)
+print("Fourth cat have some interesting characteristics -", cat4.characteristics)
+print("Btw second cat have a really cute name -", cat2.characteristics)
