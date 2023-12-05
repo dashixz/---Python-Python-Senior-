9f9182981b0016c7383220a64d975785c15118df
dashixss <dasaradomska@gmail.com>
2023-12-05 16:51:26 +0200

    1st step

---
+++ lesson3.py
@@ -0,0 +1,9 @@
+class Ppl:
+    def __init__(self, name = "Ppl", job = None, home = None, car = None):
+        self.name = name
+        self.money = 50_000
+        self.gladness = 50
+        self.satiety = 50
+        self.job = job
+        self.car = car
+        self.home = home
