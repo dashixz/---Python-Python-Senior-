10f3c0d3e5dec49f324734b4f512e6171728dd28
dashixss <dasaradomska@gmail.com>
2023-12-07 17:12:19 +0200

    2

--- lesson4.py
+++ lesson4.py
@@ -1,7 +1,18 @@
-class Parent:
-    pass
+class Grandparent:
+    height = 170
+    eyes = "light blue"
+    age = 70
+class Parent(Grandparent):
+    age = 40
 class Child(Parent):
-    pass
+    height = 165
+    age = 15
+    def __init__(self):
+        print(self.height)
+        print(self.eyes)
+        print(self.age)
+
+eric = Child()
 class Human:
     height = 170
 class Student(Human):
@@ -11,3 +22,5 @@ class Worker(Human):
 nick = Student()
 kate = Worker()
 print(nick.height, kate.height)
+
+
