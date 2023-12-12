48bb677a13c3a876a48e40b0ba3d90e1eaae64b6
dashixss <dasaradomska@gmail.com>
2023-12-12 16:59:39 +0200

    first steps

---
+++ lesson5.py
@@ -0,0 +1,12 @@
+import requests
+help(requests)
+def first_function():
+    pass
+class Human:
+    pass
+rq = requests
+first_f = first_function
+nick = Human
+print(requests.__name__)
+print(rq.__name__)
+print(nick.__name__)
