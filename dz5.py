249bad381a092bcc8f177693a9310a73679d4c71
dashixss <dasaradomska@gmail.com>
2023-12-13 19:06:17 +0200

    added background

--- dz5.py
+++ dz5.py
@@ -5,3 +5,7 @@ colorama.init()
 print(f"{colorama.Fore.RED}Цей текст червоний{colorama.Style.RESET_ALL}")
 #Додала кольоровий текст
 
+print(f"{colorama.Back.GREEN}Зелений фон{colorama.Style.RESET_ALL}")
+#Зробила колір фону колір фону
+
+
