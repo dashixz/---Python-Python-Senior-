5493fe122bb3963e17b55bb627ad4c52b204bcfc
dashixss <dasaradomska@gmail.com>
2023-12-19 17:22:07 +0200

    BuildingError

--- lesson6.py
+++ lesson6.py
@@ -9,6 +9,22 @@ try:
 except ValueError as e:
     print(e)
 
+
 text = input("Enter text")
 if type(text) != str:
     raise TypeError(f"We cant work with {type(text)} type")
+
+
+class BuildingError(Exception):
+    def __str__(self):
+        return "Not enought materials"
+
+def check_materials(amont_materials, limit_materials):
+    if amont_materials > limit_materials:
+        print("Enought materials!")
+    else:
+        raise(BuildingError)
+
+
+my_materials = 123
+check_materials(my_materials, 300)
